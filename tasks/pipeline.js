3cfa75192c91f8e05ebe5ed5ed6e06808968acef
Youssef Boudiba <youssef.boudiba@hotmail.com>
2016-11-18 14:45:14 +0100

    TODO: add bootstarp locally/ remove CDN

--- tasks/pipeline.js
+++ tasks/pipeline.js
@@ -29,8 +29,6 @@ var jsFilesToInject = [
   'js/dependencies/sails.io.js',
 
   // Dependencies like jQuery, or Angular are brought in here
-  'js/dependencies/angular.min.js',
-  'js/dependencies/angular-ui-router.min.js',
   'js/dependencies/**/*.js',
 
   // All of the rest of your client-side js files
