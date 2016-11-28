9b3ff16560061cf3344a717798e69a77d426957e
Linny123 <lindadc90@gmail.com>
2016-11-28 18:48:41 +0100

    Map toegevoegd
    Er staat nu een kaart op het tweede navigatieblad.
    Locatie aanklikken volgt later

--- config/auth.js
+++ config/auth.js
@@ -3,5 +3,10 @@ module.exports = {
         'clientID'      : '1125770550805389', // your App ID
         'clientSecret'  : '0cc7d3e07d32951bd64b976dfc46be01', // your App Secret
         'callbackURL'   : 'http://localhost:1337/auth/facebook/callback'
-    }
+	},
+
+	'GoogleApi' : {
+		'Api_key'		: 'AIzaSyBrriX06otU67RMzucGE-vRA_W4Rvvva5Y'
+	}
+
 };
