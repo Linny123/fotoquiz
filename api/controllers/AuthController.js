8f59944825c4e81873d5cdf2fde16eb5b565eef6
Youssef Boudiba <youssef.boudiba@hotmail.com>
2016-12-04 00:21:18 +0100

    Added encryption and working on JSON Web Token

--- api/controllers/AuthController.js
+++ api/controllers/AuthController.js
@@ -15,28 +15,25 @@ module.exports = {
         if(!req.body.username || !req.body.password){
             return res.status(400).json(
             {message: 'Please fill out all fields'});
-        }
-        
+        }        
         passport.authenticate('local-signup', function(err, user, next) {
             if(err){return next(err);}
             if(user){
-                return res.json({token: user.generateJWT()});
+                return res.json({token: JSONWebToken.generateJWT(user.username)});
             }
             else {return res.status(401).json(next);}
         })(req, res);
     },
 
     login: function(req, res, next) {
-
         if(!req.body.username || !req.body.password){
             return res.status(400).json(
             {message: 'Please fill out all fields'});
         }
-
         passport.authenticate('local-login', function(err, user, next) {
             if(err){return next(err);}
             if(user){
-                return res.json({token: user.generateJWT()});
+                return res.json({token: JSONWebToken.generateJWT(user.username)});
             }
             else {return res.status(401).json(next);}
         })(req, res);
@@ -52,7 +49,7 @@ module.exports = {
     facebookLoginCallback: function(req,res,next){
         passport.authenticate('facebook', function(err, user, next){
             if(user){
-                return res.redirect('/#/facebook/token=' + user.generateJWT());
+                return res.redirect('/#/facebook/token=' + JSONWebToken.generateJWT(user.username));
             }
             else {return res.status(401).json(next);}
         })(req, res);
--- api/models/User.js
+++ api/models/User.js
@@ -1,8 +1,9 @@
 // A model represents a collection of structured data
 // User.js = The set of users registered in our Database.
 
-// jsonwebtoken = method for representing claims securely between two parties
-var jwt = require('jsonwebtoken');
+
+// Encrypting password with bcrypt
+var bcrypt = require('bcryptjs');
 
 module.exports = {
     attributes: {
@@ -21,24 +22,38 @@ module.exports = {
         email: {
             type: 'string'
         },
-        // TODO ENCRYPTION
-        password: {
+        // Password is stored encrypted
+        encryptedPassword: {
             type: 'string'
         },
         FBID: {
             type: 'string'
         },
-        // Token will be used to authenticate user and manage sessions (via expiration)
-        generateJWT: function() {
-            var today = new Date();
-            var exp = new Date(today);
-            exp.setDate(today.getDate() + 1);
-
-            return jwt.sign({
-            username: this.username,
-            exp: parseInt(exp.getTime() / 1000)
-            }, 'SECRET');
+        // Password will never be exchanged
+        generateToken: function() {
+          var token = this.toObject();
+          delete token.encryptedPassword;
+          return token;            
         }
-        // TODO ENCRYPTION generateHash + comparepassword
+    },
+    beforeCreate : function (values, next) {
+        bcrypt.genSalt(10, function (err, salt) {
+            if(err) return next(err);
+            bcrypt.hash(values.password, salt, function (err, hash) {
+                if(err) return next(err);
+                values.encryptedPassword = hash;
+                next();
+            })
+        })
+    },
+    comparePassword : function (password, user, callback) {
+        bcrypt.compare(password, user.encryptedPassword, function (err, match) {
+            if(err) callback(err);
+            if(match) {
+                callback(null, true);
+            } else {
+                callback(err);
+            }
+        })
     }
 };
