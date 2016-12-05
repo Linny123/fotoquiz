5dbc538718d496411cdca22e0228c838e6df08d5
Linny123 <lindadc90@gmail.com>
2016-12-05 22:07:34 +0100

    Img uitlezen
    afbeelding van een bepaald ID kan nu worden opgevraagd bij de imgur
    server

--- assets/js/services/imgur.js
+++ assets/js/services/imgur.js
@@ -1,9 +1,10 @@
+var ClientID = '90f58e42cf3f991';
 
 function addImage(img){
     $.ajax({ 
         url: 'https://api.imgur.com/3/image',
         headers: {
-            'Authorization': 'Client-ID 90f58e42cf3f991'
+            'Authorization': 'Client-ID ' +ClientID
         },
         type: 'POST',
         data: {
@@ -11,6 +12,18 @@ function addImage(img){
         },
         success: function() { console.log('cool'); }
     });
+};
 
-
-}
+function getImage(id){
+    $.ajax({ 
+        url: 'https://api.imgur.com/3/image/'+ id,
+        headers: {
+            'Authorization': 'Client-ID ' +ClientID
+        },
+        type: 'GET',
+        data: {
+            'image': 'img'
+        },
+        success: function() { console.log('cool'); }
+    });
+};
