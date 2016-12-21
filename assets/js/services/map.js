c7bec4baa672909362bba45d0528718b4974172e
Thibaut <Thibaut@MacBook-Pro.local>
2016-12-21 01:07:21 +0100

    Cleanup of map.js
    - Rewrote some functions in map.js
    - Removed a lot of unnecessary lines of code and functions
    - Work in progress!

--- assets/js/services/map.js
+++ assets/js/services/map.js
@@ -1,13 +1,13 @@
 var map;
 var chances = 0;
 var win = false;
-var point = {lat: 50.8201646, lng: 4.398042};
+//var point = {lat: 50.8201646, lng: 4.398042};
 var latlng;
-var options;
-var quizID; // the quiz ID
+//var options;
+//var quizID; // the quiz ID
 var latitude = null;
 var longitude = null;
-var points = 0; // points earned
+//var points = 0; // points earned
 var newQuizMarker;
 
 // Transforms an address/location (in text form) to latlng 
@@ -54,6 +54,7 @@ var getDistance = function(p1, p2) {
   return d; // returns the distance in meter
 };
 
+
 // Loads the map and centers + places marker on the given latlng
 function loadMap(lat, lng) {
   if(lat == null) {
@@ -63,7 +64,7 @@ function loadMap(lat, lng) {
   }
   console.log("Create new map and center, lat: "+ lat+ "lng: "+lng+" LOCATION:"+location);
 
-  options = {
+  var options = {
       zoom: 13,
       center: location,
       mapTypeId: google.maps.MapTypeId.ROADMAP
@@ -81,75 +82,59 @@ function loadMap(lat, lng) {
 
 
 
-
-
-
 function GuessingMap(){
-    
-      geocoder = new google.maps.Geocoder();
-      //fotosID = fotoID
-      
-      if(arguments.length == 0){
-        // provide some default initialization options
-        options = 
-        {
-          zoom: 14,
-          center: new google.maps.LatLng(50.8201646, 4.398042),
-          mapTypeId: google.maps.MapTypeId.ROADMAP
+  // Create map
+  var options = {
+    zoom: 10,
+    center: new google.maps.LatLng(50.8201646, 4.398042), // Center on VUB
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+  };
+
+  map = new google.maps.Map(document.getElementById("map"), options);
+
+  // Ask for HTML5 user location (does not work on Safari 10 if HTTP)
+  var onError = function(error) {
+    console.log("Could not get the current location. Error: "+error);
+  };
+
+  if(navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(function(position) {
+        var pos = {
+          lat: position.coords.latitude,
+          lng: position.coords.longitude
         };
-      }
       
-      // map_canvas is the id of the HTML element we are using as the map canvas (see HTML snippet above)
-      map = new google.maps.Map(document.getElementById("map"), options);
+        map.panTo(pos);
 
-      var onError = function(error) {
-        console.log("Could not get the current location.");
-      };
-      
-      if(navigator.geolocation) {
-         navigator.geolocation.getCurrentPosition(
-          function(position) {
-           var currentLocation = new google.maps.LatLng(position.coords.latitude,position.coords.longitude);
-            // now that we have the current location, use the Map API to move the map to this location
-            // you may also want to adjust the zoom level
-            // see the API reference for the relevant functions:
-            // https://developers.google.com/maps/documentation/javascript/reference
-            
-            map.panTo(currentLocation);
-            
-          }, 
-          onError
-       );
-      }else{
-        onError();
-      }
-      google.maps.event.addListener(map, "click", Testlocation);
+      }, onError);
 
-      
+  } else {
+    onError();
+  }
+
+  // Add guess click function
+  google.maps.event.addListener(map, "click", Testlocation);    
 }
 
 
 
+// function getlocation(event){
+//   if(chances > 0){
+//     console.log("no");
+//   } else {
+//     var marker = new google.maps.Marker({ 
+//       map: map,
+//       position: event.latLng,
+//       title: "MARKER"
+//     });
 
+//     chances = chances+1;
+//     latlng = event.latLng;
+//   }  
+//   latitude = latlng.lat();
+//   longitude = latlng.lng();
 
-function getlocation(event){
-  if(chances > 0){
-    console.log("no");
-  }else{
-    var marker = new google.maps.Marker(
-    { 
-      map: map,
-      position: event.latLng,
-      title: "NEW MARKER"
-    });
-  google.maps.event.addListener(marker, "click", function(event){openMarkerPopup(marker);});
-  chances = chances+1;
-  latlng = event.latLng;
-  }  
-  latitude = latlng.lat();
-  longitude = latlng.lng();
-
-}
+// }
 
 function Testlocation(event){
   var loc = new google.maps.LatLng(latitude, longitude);
@@ -160,9 +145,8 @@ function Testlocation(event){
     { 
       map: map,
       position: event.latLng,
-      title: "NEW MARKER"
+      title: "A GUESS"
     });
-  google.maps.event.addListener(marker, "click", function(event){openMarkerPopup(marker);});
 
     // testing location smaller then 100 m
     if(getDistance(loc, latlng) < 100){
